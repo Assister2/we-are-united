109539f78e5bda2e01208b27e84d1f5a10a3f9cb
Assister2 <staniwitkow@gmail.com>
2023-08-21 00:40:36 +0300

    eng finish

--- src/components/textItem.js
+++ src/components/textItem.js
@@ -4,15 +4,18 @@ import {
     Box,
     Text,
   } from "@chakra-ui/react";
+import {useTranslation} from "react-i18next";
 
 function TextItem({about1 , wei}) {
-   const font20 = {lg:'20px', md:'16px', sm:'12px', base:'8px'};
-   const font32 = {lg:'32px', md:'28px', base:'24px', sm:'24px'};
-   return (
-        <Text textAlign={"justify"} fontSize={font20}
-        fontFamily={"Montserrat"} fontStyle={"normal"} fontWeight={wei}
-        lineHeight={"40px"} letterSpacing={"0.2px"} font-variant="small-caps"
-        >{about1}</Text>
+  const {t, i18n} = useTranslation('common');
+  const font20 = {lg:'20px', md:'16px', sm:'12px', base:'8px'};
+  const font32 = {lg:'32px', md:'28px', base:'24px', sm:'24px'};
+  console.log("t",t, "about", about1)
+  return (
+    <Text textAlign={"justify"} fontSize={font20}
+    fontFamily={"Montserrat"} fontStyle={"normal"} fontWeight={wei}
+    lineHeight={"40px"} letterSpacing={"0.2px"} font-variant="small-caps"
+    >{t(about1)}</Text>
   );
 }
 
--- src/components/textItem2.js
+++ src/components/textItem2.js
@@ -1,11 +1,13 @@
 import React from "react";
 import {
-    Container,
-    Box,
-    Text,
-  } from "@chakra-ui/react";
+  Container,
+  Box,
+  Text,
+} from "@chakra-ui/react";
+import {useTranslation} from "react-i18next";
 
 function TextItem2({about1, about2}) {
+  const {t, i18n} = useTranslation('common');
    const font20 = {lg:'20px', md:'16px', sm:'12px', base:'8px'};
    const font32 = {lg:'32px', md:'28px', base:'24px', sm:'24px'};
    return (
@@ -14,11 +16,11 @@ function TextItem2({about1, about2}) {
         fontFamily={"Montserrat"} fontStyle={"normal"} fontWeight={700}
         lineHeight={"40px"} letterSpacing={"0.2px"} font-variant="small-caps"
         mb={5}
-        >{about1}</Text>
+        >{t(about1)}</Text>
         <Text textAlign={"justify"} fontSize={font20}
         fontFamily={"Montserrat"} fontStyle={"normal"} fontWeight={600}
         lineHeight={"40px"} letterSpacing={"0.2px"} font-variant="small-caps">
-        {about2}
+        {t(about2)}
         </Text>
       </>
   );
