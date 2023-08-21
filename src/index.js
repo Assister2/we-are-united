910564ce231793fff123fd6fbd61b4aab51673d3
Assister2 <staniwitkow@gmail.com>
2023-08-21 08:43:55 +0300

    germany and espanol

--- src/index.js
+++ src/index.js
@@ -10,18 +10,30 @@ import {I18nextProvider} from "react-i18next";
 import i18next from "i18next";
 import common_en from "./translations/en/common.json";
 import common_de from "./translations/de/common.json";
+import common_fr from "./translations/fr/common.json";
+import common_es from "./translations/es/common.json";
+import common_it from "./translations/it/common.json";
 
 const root = createRoot(document.getElementById("root"));
 i18next.init({
   interpolation: { escapeValue: false },  // React already does escaping
-  lng: 'en',                              // language to use
+  lng: 'ua',                              // language to use
     resources: {
         en: {
-            common: common_en               // 'common' is our custom namespace
-        }
-        // de: {
-        //     common: common_de
+          common: common_en               // 'common' is our custom namespace
+        },
+        de: {
+          common: common_de
+        },
+        // fr: {
+        //   common: common_fr
         // },
+        es: {
+          common: common_es
+        },
+        // it: {
+        //   common: common_it
+        // }
     },
 });
 
